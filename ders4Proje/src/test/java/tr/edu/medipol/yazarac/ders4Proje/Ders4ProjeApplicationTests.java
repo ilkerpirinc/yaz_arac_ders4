b40d45ca3aa22b3813862195365b4a900ad3ac93
bprlab <bprlab@medipolyon-imac-25.local>
2018-11-10 03:06:59 -0800

    spring boottan proje oluşturduk. derste yapılan örnek

---
+++ ders4Proje/src/test/java/tr/edu/medipol/yazarac/ders4Proje/Ders4ProjeApplicationTests.java
@@ -0,0 +1,16 @@
+package tr.edu.medipol.yazarac.ders4Proje;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Ders4ProjeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
